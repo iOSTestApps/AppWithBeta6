8ccc0daba476a29138ac4f5e952d862f50aaf5a9
Romain Pouclet <romain.pouclet@gmail.com>
2018-08-21 09:12:16 -0700

    Initial Commit

---
+++ AppWithBeta6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppWithBeta6
+//
+//  Created by Romain Pouclet on 2018-08-21.
+//  Copyright © 2018 Buddybuild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
